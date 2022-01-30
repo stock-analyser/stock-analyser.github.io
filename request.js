d8c905dab16a1d41ad0e3f3d80ae9eac4e7d6bb0
stock-analyser <98676257+stock-analyser@users.noreply.github.com>
2022-01-30 17:02:01 +0800

    Update request.js

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
 const request = new XMLHttpRequest();
-request.open("GET", "https://q.stock.sohu.com/hisHq?code=cn_601766&start=20150601&end=20210414");
+request.open("GET", "http://cors.io/?https://q.stock.sohu.com/hisHq?code=cn_601766&start=20150601&end=20210414");
 request.send();
 request.onload = () => {
     if (request.status === 200) {
